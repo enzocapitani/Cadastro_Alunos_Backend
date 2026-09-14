b49849c19907d6e600b2667e2ad99fdf867c2d45
Enzo Capitani <enzocapitanivieira@gmail.com>
2026-09-14 10:21:02 -0300

    feat: criação do controller para receber as requisições http

---
+++ src/main/java/org/example/cadastroalunobackend/controller/AlunoController.java
@@ -0,0 +1,29 @@
+package org.example.cadastroalunobackend.controller;
+
+import org.example.cadastroalunobackend.model.Aluno;
+import org.example.cadastroalunobackend.service.AlunoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/alunos")
+@RestController
+public class AlunoController {
+
+    private final AlunoService alunoService;
+
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
+
+    @PostMapping
+    public void adicionarAluno(@RequestBody Aluno aluno){
+        alunoService.adicionarAluno(aluno);
+    }
+
+    @GetMapping
+    public List<Aluno> verAlunos(){
+        return alunoService.verAlunos();
+    }
+
+}
