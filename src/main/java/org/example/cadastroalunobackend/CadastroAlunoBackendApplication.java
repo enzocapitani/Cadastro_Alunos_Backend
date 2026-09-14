65ae191d9edde1df4d523b1c83919cfb1409df7a
Enzo Capitani <enzocapitanivieira@gmail.com>
2026-09-14 10:20:02 -0300

    feat: criação do codigo fonte springboot

---
+++ src/main/java/org/example/cadastroalunobackend/CadastroAlunoBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.cadastroalunobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroAlunoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroAlunoBackendApplication.class, args);
+    }
+
+}
