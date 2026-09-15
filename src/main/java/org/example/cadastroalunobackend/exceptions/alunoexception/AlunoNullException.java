1f75f31dbd32fef86f7505d45af590a5502f6836
Enzo Capitani <enzocapitanivieira@gmail.com>
2026-09-15 09:59:08 -0300

    feat: criação de exceções personalizadas para a criação de alunos

---
+++ src/main/java/org/example/cadastroalunobackend/exceptions/alunoexception/AlunoNullException.java
@@ -0,0 +1,9 @@
+package org.example.cadastroalunobackend.exceptions.alunoexception;
+
+public class AlunoNullException extends RuntimeException{
+
+    public AlunoNullException(String message){
+        super(message);
+    }
+
+}
---
+++ src/main/java/org/example/cadastroalunobackend/exceptions/alunoexception/AlunoSemBairroException.java
@@ -0,0 +1,9 @@
+package org.example.cadastroalunobackend.exceptions.alunoexception;
+
+public class AlunoSemBairroException extends RuntimeException {
+
+    public AlunoSemBairroException(String message){
+        super(message);
+    }
+
+}
