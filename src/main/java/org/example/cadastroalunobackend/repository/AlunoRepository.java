9eedb80ed46939da3fedd0523a0e66dc84115cf0
Enzo Capitani <enzocapitanivieira@gmail.com>
2026-09-24 10:02:13 -0300

    refactor: agora alunos são retornados por ordem alfabética

--- src/main/java/org/example/cadastroalunobackend/repository/AlunoRepository.java
+++ src/main/java/org/example/cadastroalunobackend/repository/AlunoRepository.java
@@ -2,6 +2,13 @@ package org.example.cadastroalunobackend.repository;
 
 import org.example.cadastroalunobackend.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+
+    @Query("SELECT a FROM Aluno a ORDER BY a.nome, a.curso")
+    List<Aluno> retornarOrdenado();
+
 }
